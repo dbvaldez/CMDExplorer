be32fb4ec02eb12c723b047bd249c862bb997024
Dave Valdez <david.b.valdez@gmail.com>
2025-07-17 17:43:24 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+targets = {
+    "Pleiades": {"ra": 56.75, "dec": 24.12, "radius": 1.0},
+    "Hyades": {"ra": 66.75, "dec": 15.87, "radius": 1.0},
+    "Galactic Plane": {"ra": 200.0, "dec": 0.0, "radius": 1.0},
+}
