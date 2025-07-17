55421beb8c9e92c6e2c47d38bf774e4940daa438
Dave Valdez <david.b.valdez@gmail.com>
2025-07-17 17:40:33 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,19 +1,20 @@
-import numpy as np
-import pandas as pd
 from astroquery.gaia import Gaia
+import pandas as pd
 
-def query_region(ra, dec, radius_deg):
+def query_region(ra, dec, radius_deg, row_limit=5000):
     query = f"""
-    SELECT source_id, ra, dec, parallax, phot_g_mean_mag,
+    SELECT TOP {row_limit} source_id, ra, dec, parallax, phot_g_mean_mag,
            bp_mean_mag, rp_mean_mag
     FROM gaiadr3.gaia_source
     WHERE CONTAINS(POINT('ICRS', ra, dec),
                    CIRCLE('ICRS', {ra}, {dec}, {radius_deg})) = 1
     """
-    job = Gaia.launch_job(query)
-    return job.get_results().to_pandas()
+    try:
+        Gaia.MAIN_GAIA_SERVER = "https://gea.esac.esa.int"
+        Gaia.verbose = True
+        job = Gaia.launch_job(query)
+        return job.get_results().to_pandas()
+    except Exception as e:
+        print(f"Query failed: {e}")
+        return pd.DataFrame()
 
-def preprocess(df):
-    df['bp_rp'] = df['bp_mean_mag'] - df['rp_mean_mag']
-    df['abs_mag'] = df['phot_g_mean_mag'] - 5 * (np.log10(1000 / df['parallax']) - 1)
-    return df
