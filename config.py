8bd006c6ec7978056aac9a2fe489c9fed412ec4a
Dave Valdez <david.b.valdez@gmail.com>
2025-07-17 18:00:10 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 targets = {
-    "Pleiades": {"ra": 56.75, "dec": 24.12, "radius": 2.0},
-    "Hyades": {"ra": 66.75, "dec": 15.87, "radius": 2.0},
-    "Galactic Plane": {"ra": 200.0, "dec": 0.0, "radius": 2.0},
+    "Pleiades": {"ra": 56.75, "dec": 24.12, "radius": 0.5},
+    "Hyades": {"ra": 66.75, "dec": 15.87, "radius": 0.5},
+    "Galactic Plane": {"ra": 200.0, "dec": 0.0, "radius": 0.5},
 }
